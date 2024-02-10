612a32f7846782c058907a7cc22741ab0ea47d32
Kai Wium-Andersen <62029204+KaiWiumAndersen@users.noreply.github.com>
2024-02-10 11:00:36 +0100

    Netzwerk_Sender.cpp

--- Netzwerk_Sender.cpp
+++ Netzwerk_Sender.cpp
@@ -1,37 +1,57 @@
 #include <iostream>
-#include <boost/asio.hpp>
-#include <thread>
+#include <winsock2.h>
+#include <wS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 using namespace std;
-using namespace boost::asio;
-using namespace std::chrono;
+
 int main() {
-	try {
-		string Nachricht1 = "Hallo Programm 1";
-		string Nachricht2 = "Mir geht es sehr gut";
-		cout << "Sender" << endl;
-		io_service IO;
-		ip::tcp::socket socket(IO); 
-		ip::tcp::endpoint endpoint(ip::address::from_string("127.0.0.1"), 1122); 
-
-		system("Path\\Netzwerk_Empfänger");
-		this_thread::sleep_for(milliseconds(500));
-		cout << "start fertig" << endl;
-		this_thread::sleep_for(milliseconds(500));
-		system("start /d");
-		this_thread::sleep_for(milliseconds(3500));
-		cout << "/d fertig" << endl;
-		this_thread::sleep_for(milliseconds(500));
-		socket.connect(endpoint);
-
-		write(socket, buffer(Nachricht1));
-		write(socket, buffer(Nachricht2));
-
-		char Data1[1024];
-		size_t Data2 = socket.read_some(buffer(Data1));
-		Data1[Data2];
-		cout << "Empfangene Daten vom Sender: " << Data1 << endl;
+	WSADATA WD;
+	if (WSAStartup(MAKEWORD(2, 2), &WD) != 0) {
+		cout << "WSAStartup ist fehlgeschlagen: " << WSAGetLastError() << endl;
+		return 1;
 	}
-	catch (exception& Ausnahme) {
-		cout << "Empfänger Ausnahme: " << Ausnahme.what() << endl;
+	else {
+		cout << "WSAStartup war erfolgreich" << endl;
 	}
+	SOCKET Socket1 = socket(AF_INET, SOCK_STREAM, 0);
+	if (Socket1 == INVALID_SOCKET) {
+		cout << "Socket1 ist fehlgeschlagen: " << WSAGetLastError() << endl; 
+		WSACleanup();
+		return 1;
+	}
+	else {
+		cout << "Socket1 war erfolgreich" << endl; 
+	}
+
+	sockaddr_in ServerAdress;
+	ServerAdress.sin_family = AF_INET;
+	ServerAdress.sin_port = htons(4444);
+	if (inet_pton(AF_INET, "127.0.0.1", &(ServerAdress.sin_addr)) <= 0) {
+		cout << "fehlerhafte IP-Adresse: " << WSAGetLastError();
+		closesocket(Socket1);
+		WSACleanup();
+	}
+	else {
+		cout << "fehlerfreie IP-Adresse" << endl;
+	}
+	if (connect(Socket1, (struct sockaddr*)&ServerAdress, sizeof(ServerAdress)) == SOCKET_ERROR) {  
+		cout << "Verbindung ist fehlgeschlagen: " << WSAGetLastError() << endl;
+		closesocket(Socket1);
+		WSACleanup();
+		return 1;
+	}
+	else {
+		cout << "Verbind war erfolgreich" << endl;
+	}
+	char Buffer[1024];
+	string Nachricht1 = "Hallo Server!";
+	if (send(Socket1, Nachricht1.c_str(), Nachricht1.size(), 0) == SOCKET_ERROR) {
+		cout << "Nachricht wurde nicht erfolgreich geschickt: " << WSAGetLastError() << endl;
+	}
+	else {
+		cout << "Nachricht wurde erfolgreich verschickt" << endl; 
+	}
+	closesocket(Socket1);
+	WSACleanup();
+	return 0;
 }
